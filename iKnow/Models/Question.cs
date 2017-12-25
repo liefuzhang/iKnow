1a1e0be85b8e0201d1ff3f45a03b2eed33412434
liefu zhang <liefuzhangnz@gmail.com>
2017-12-25 22:58:40 +1300

    Initial checkin

---
+++ iKnow/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iKnow.Models {
+    public class Question {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
