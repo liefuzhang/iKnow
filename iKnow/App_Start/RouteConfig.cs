b65762c3239c35f442cb8f860cbd19844e4205eb
liefu zhang <liefuzhangnz@gmail.com>
2017-12-26 23:39:05 +1300

    topic started

--- iKnow/App_Start/RouteConfig.cs
+++ iKnow/App_Start/RouteConfig.cs
@@ -9,6 +9,7 @@ namespace iKnow {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
 
             routes.MapRoute(
                 name: "Default",
