7cb322ad2147990d69e33477ab22eeea6892afb4
leif23 <liefuzhangnz@gmail.com>
2017-12-27 15:53:58 +1300

    Topic selection

--- iKnow/Controllers/TopicController.cs
+++ iKnow/Controllers/TopicController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using iKnow.Models;
 
 namespace iKnow.Controllers
 {
@@ -35,6 +36,38 @@ namespace iKnow.Controllers
         public ActionResult Detail(int id) {
 
             return View();
+        }
+
+        // GET: Topic/About/1
+        public PartialViewResult About(int id) {
+            var topic = _context.Topics.SingleOrDefault(t => t.Id == id);
+            if (topic == null) {
+                return null;
+            }
+
+            return PartialView("_TopicBodyPartial", topic);
+        }
+
+        // GET: Topic/Add
+        public ActionResult Add() {
+            var topic = new Topic();
+
+            return View(topic);
+        }
+
+        // POST: Topic/Save
+        [HttpPost]
+        public ActionResult Save(Topic topic) {
+            if (!ModelState.IsValid) {
+                return View("Add");
+            }
+
+            topic.Id = 0;
+            _context.Topics.Add(topic);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
--- iKnow/Scripts/site.js
+++ iKnow/Scripts/site.js
@@ -2,7 +2,22 @@
     $(".topic-list").on("click", "li", selectTopic);
 });
 
-function selectTopic(e) {
-    var topicId = $(this).attr("data-topic-id");
+function selectTopic() {
+    var $this = $(this);
+    if ($this.hasClass("active")) {
+        return;
+    }
 
+    var topicId = $this.attr("data-topic-id");
+    $.ajax({
+        url: "topic/about/" + topicId,
+        dataType: "html",
+        success: function (html) {
+            if (html) {
+                $this.siblings().removeClass("active");
+                $this.addClass("active");
+                $(".topic-body").html(html);
+            }
+        }
+    });
 }
