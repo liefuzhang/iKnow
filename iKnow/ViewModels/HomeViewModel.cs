3fc0efe6815938552c399987d75a6e12e689fa67
liefu zhang <liefuzhangnz@gmail.com>
2018-01-03 18:24:16 +1300

    added missing files

---
+++ iKnow/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iKnow.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace iKnow.ViewModels {
+    public class HomeViewModel {
+        public IDictionary<Question, Answer> QuestionAnswers { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ iKnow/ViewModels/TopicDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iKnow.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace iKnow.ViewModels {
+    public class TopicDetailViewModel {
+        public Topic Topic { get; set; }
+        public IDictionary<Question, Answer> QuestionAnswers { get; set; }
+    }
+}
