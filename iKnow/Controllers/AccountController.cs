d64b6093e2f568c30668721b3521830e88b63c9b
liefu zhang <liefuzhangnz@gmail.com>
2018-01-10 20:55:38 +1300

    added error pages

--- iKnow/Controllers/AccountController.cs
+++ iKnow/Controllers/AccountController.cs
@@ -213,7 +213,6 @@ namespace iKnow.Controllers {
         //
         // POST: /Account/LogOff
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult LogOff() {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
             return RedirectToAction("Index", "Home");
