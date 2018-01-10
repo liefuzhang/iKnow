0c9bf7323fe3311300f94982b371ce49701efcd3
liefu.zhang <liefuzhangnz@gmail.com>
2018-01-10 19:17:14 +1300

    error page

---
+++ iKnow/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iKnow.Controllers
+{
+    public class ErrorController : Controller {
+        public ActionResult Index() {
+            return View("Error");
+        }
+
+        public ActionResult NotFound() {
+            Response.StatusCode = 200;
+            return View("404");
+        }
+    }
+}
